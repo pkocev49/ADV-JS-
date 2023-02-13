28f6a5311d367ea6a79b48f8240dd6edbc27451b
pkocev49 <120195818+pkocev49@users.noreply.github.com>
2023-02-13 14:17:37 +0100

    WORK SHOP ADV JS #1

--- WORK SHOP ADV JS #1/app.js
+++ WORK SHOP ADV JS #1/app.js
@@ -1,81 +1,123 @@
-let peopleImage = document.querySelector("#people_img")
-let shipsImage = document.querySelector('#ships_img')
-let nextBtn = document.querySelector('#next_button')
-let previousBtn = document.querySelector('#back_button')
+let peopleImage = document.querySelector("#people_img");
+let shipsImage = document.querySelector("#ships_img");
+let nextBtn = document.querySelector("#next_button");
+let previousBtn = document.querySelector("#back_button");
+let previousShips_btn = document.querySelector("#previousShips_button");
+let nextShips_btn = document.querySelector("#nextShips_button");
+let closeButton = document.querySelector(".close");
+let tableWindow = document.querySelector(".table_div");
+let shadow = document.querySelector(".overlay");
+let h1TextS = document.querySelector(".h1Ships");
+let buttonDiv = document.querySelector(".button_div");
+let h1Text = document.querySelector(".h1People");
+let secondButtonDiv = document.querySelector(".secondButton_div");
 
-peopleImage.addEventListener('click', getPeopleFetch)
-nextBtn.addEventListener('click', next)
-previousBtn.addEventListener('click', previous)
+peopleImage.addEventListener("click", getPeopleFetch);
+shipsImage.addEventListener("click", getShipsFetch);
+nextBtn.addEventListener("click", nextPeople);
+previousBtn.addEventListener("click", previousPeople);
+previousShips_btn.addEventListener("click", previousShips);
+nextShips_btn.addEventListener("click", nextShips);
+closeButton.addEventListener("click", closeBtn);
 
-let peopleurl = "https://swapi.dev/api/people/?page="
-let shipsUrl = "https://swapi.dev/api/starships/?page="
-let pageNumber = 1
+let peopleurl = "https://swapi.dev/api/people/?page=";
+let shipsUrl = "https://swapi.dev/api/starships/?page=";
+let pageNumber = 1;
 
-
-
-
-function previous() {
-   pageNumber--
-   fetch(`${peopleurl}${pageNumber}`)
-      .then(function (res) {
-         return res.json()
-      })
-      .then(function (data) {
-         printPeople(mainEl, data)
-      })
-   nextBtn.style.display = 'block'
-   if (pageNumber === 1) {
-      previousBtn.style.display = 'none'
-   }
+function previousPeople() {
+  pageNumber--;
+  fetch(`${peopleurl}${pageNumber}`)
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      printPeople(mainEl, data);
+    });
+  nextBtn.style.display = "block";
+  if (pageNumber === 1) {
+    previousBtn.style.display = "none";
+  }
 }
 
-function next() {
-   pageNumber++
-   fetch(`${peopleurl}${pageNumber}`)
-      .then(function (res) {
-         return res.json()
-      })
-      .then(function (data) {
-         printPeople(mainEl, data)
-      })
-
-   if (pageNumber === 9) {
-      nextBtn.style.display = 'none'
-   } else (pageNumber < 1)
-   previousBtn.style.display = 'block'
-
-
-
-
+function previousShips() {
+  pageNumber--;
+  fetch(`${shipsUrl}${pageNumber}`)
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      printShips(mainEl, data);
+    });
+  nextShips_btn.style.display = "block";
+  if (pageNumber === 1) {
+    previousShips_btn.style.display = "none";
+  }
 }
 
+function nextPeople() {
+  pageNumber++;
+  fetch(`${peopleurl}  ${pageNumber}`)
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      printPeople(mainEl, data);
+    });
 
+  if (pageNumber === 9) {
+    nextBtn.style.display = "none";
+  } else pageNumber < 1;
+  previousBtn.style.display = "block";
+}
+function nextShips() {
+  pageNumber++;
+  fetch(`${shipsUrl}  ${pageNumber}`)
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      printShips(mainEl, data);
+    });
 
+  if (pageNumber === 4) {
+    nextShips_btn.style.display = "none";
+  } else pageNumber < 1;
+  previousShips_btn.style.display = "block";
+}
+
+function getShipsFetch() {
+  fetch(`${shipsUrl}${pageNumber}`)
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      printShips(mainEl, data);
+    });
+
+  tableWindow.style.display = "block";
+  shadow.style.display = "block";
+  h1TextS.style.display = "block";
+  buttonDiv.style.display = "none";
+}
 
 function getPeopleFetch() {
+  fetch(`${peopleurl}${pageNumber}`)
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      printPeople(mainEl, data);
+    });
 
-   let tableWindow = document.querySelector('.table_div')
-   let shadow = document.querySelector('.overlay')
-   let h1Text = document.querySelector('.h1People')
-
-   tableWindow.style.display = 'block'
-   shadow.style.display = 'block'
-   h1Text.style.display = "block"
-   fetch(`${peopleurl}${pageNumber}`)
-      .then(function (res) {
-         return res.json()
-      })
-      .then(function (data) {
-         printPeople(mainEl, data)
-      })
-
+  secondButtonDiv.style.display = "none";
+  tableWindow.style.display = "block";
+  shadow.style.display = "block";
+  h1Text.style.display = "block";
 }
 
-
-
 function printPeople(mainEl, peopleData) {
-   let tableContent = ""
-   mainEl = `
+  let tableContent = "";
+  mainEl = `
       <tbody>
       <tr>
          <th>Name:</th>
@@ -88,9 +130,9 @@ function printPeople(mainEl, peopleData) {
       </tbody>
 
       
-   `
-   for (people of peopleData.results) {
-      tableContent += `
+   `;
+  for (people of peopleData.results) {
+    tableContent += `
          <tr>
             <td>${people.name}</td>
             <td>${people.height}cm</td>
@@ -98,24 +140,17 @@ function printPeople(mainEl, peopleData) {
             <td>${people.birth_year}</td>
             <td>${people.gender}</td>
             <td>${people.films.length}</td>
-           
-          
          </tr>
-      `
-   }
+      `;
+  }
 
-   document.querySelector('#mainEl').innerHTML = mainEl
-   document.querySelector('table').innerHTML += tableContent
-
-
+  document.querySelector("#mainEl").innerHTML = mainEl;
+  document.querySelector("table").innerHTML += tableContent;
 }
 
-
-
-
 function printShips(mainEl, shipsData) {
-   let tableContent = ""
-   mainEl = `
+  let tableContent = "";
+  mainEl = `
       <tbody>
       <tr>
          <th>Name:</th>
@@ -126,58 +161,50 @@ function printShips(mainEl, shipsData) {
          <th>Class:</th>
       </tr>
       </tbody>
-   `
+      `;
 
-   for (ships of shipsData.results) {
+  for (ships of shipsData.results) {
+    if (ships.crew.includes(",")) {
+      ships.crew = ships.crew.split(",").join("");
+    } else if (ships.crew.includes("-")) {
+      ships.crew = ships.crew.split("-");
+      ships.crew.length;
+      ships.crew = ships.crew.reduce((num1, num2) => {
+        return parseInt(num1) + parseInt(num2) / 2;
+      });
+      console.log(ships.crew);
+    } else if (ships.crew.includes("unknown")) {
+      ships.crew = 0;
+    }
+    if (ships.passengers.includes(",")) {
+      ships.passengers = ships.passengers.split(",").join("");
+    } else if (ships.passengers.includes("unknown")) {
+      ships.passengers = 0;
+    } else if (ships.passengers.includes("n/a")) {
+      ships.passengers = 0;
+    }
 
+    let crewAndPassengers = parseInt(ships.crew) + parseInt(ships.passengers);
 
-      if (ships.crew.includes('unknown' && 'n/a')) {
-         ships.crew = 0
-
-      } if (ships.passengers.includes("unknown" && 'n/a')) {
-         ships.passengers = 0
-      }
-      let sum = ships.crew * ships.passengers
-      sum = parseInt(sum)
-      tableContent += `
+    tableContent += `
               <tr>
                  <td>${ships.name}</td>
                  <td>${ships.model}</td>
                  <td>${ships.manufacturer}kg</td>
-                 <td>${ships.cost_in_credits}credits</td>
-                 <td>${sum}</td>
+                 <td>${ships.cost_in_credits} credits</td>
+                 <td>${crewAndPassengers}</td>
                  <td>${ships.starship_class}</td>
               </tr>
-           `
-   }
-   document.querySelector('#mainEl').innerHTML = mainEl
-   document.querySelector('table').innerHTML += tableContent
+           `;
+  }
+  document.querySelector("#mainEl").innerHTML = mainEl;
+  document.querySelector("table").innerHTML += tableContent;
 }
 
-
-
-
-shipsImage.addEventListener("click", function () {
-
-   fetch(`${shipsUrl}${pageNumber}`)
-      .then(function (res) {
-         return res.json()
-      })
-      .then(function (data) {
-
-         printShips(mainEl, data)
-      })
-
-
-
-   let tableWindow = document.querySelector('.table_div')
-   let shadow = document.querySelector('.overlay')
-   let h1TextS = document.querySelector('.h1Ships')
-
-   tableWindow.style.display = 'block'
-   shadow.style.display = 'block'
-   h1TextS.style.display = 'block'
-
-
-
-})
+function closeBtn() {
+  tableWindow.style.display = "none";
+  shadow.style.display = "none";
+  secondButtonDiv.style.display = "none";
+  h1Text.style.display = "none";
+  h1TextS.style.display = "none";
+}
