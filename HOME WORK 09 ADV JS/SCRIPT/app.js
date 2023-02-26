c57649cd8248c0b2f3de1a5d09cc891e406c0c96
pkocev49 <120195818+pkocev49@users.noreply.github.com>
2023-02-26 18:43:11 +0530

    Home Work 9 finished

--- HOME WORK 09 ADV JS/SCRIPT/app.js
+++ HOME WORK 09 ADV JS/SCRIPT/app.js
@@ -1,33 +1,26 @@
-console.log('works')
+let mkdUrl = " https://restcountries.com/v3.1/alpha/";
 
-// let countryUrl = 'https://restcountries.com/v3.1/aha/MKD'
-
-
-// fetch(countryUrl)
-//     .then(res => res.json())
-//     .then(data =>{
-//         console.log(data)
-//         for(border of data){
-//             console.log(`${border.borders}`)
-//         }
-//     })
-
-async function getBorderingCountries(alphaCode) {
-    const response = await fetch(`https://restcountries.com/v3.1/alpha/${alphaCode}`);
-    const country = await response.json();
-    
-    console.log("Country:");
-    console.log(country);
-    
+const getCounttry = async (country) => {
+  const response = await fetch(`${mkdUrl}${country}`);
+  const data = await response.json();
+  console.log("Country MKD:", data);
+  data.forEach(async (border) => {
+    const responseZero = await fetch(`${mkdUrl}${border.borders[0]}`);
+    const borderData = await responseZero.json();
+    const responseOne = await fetch(`${mkdUrl}${border.borders[1]}`);
+    const BgData = await responseOne.json();
+    const responseTwo = await fetch(`${mkdUrl}${border.borders[2]}`);
+    const grcData = await responseTwo.json();
+    const responseThree = await fetch(`${mkdUrl}${border.borders[3]}`);
+    const unkData = await responseThree.json();
+    const responseFour = await fetch(`${mkdUrl}${border.borders[4]}`);
+    const srbData = await responseFour.json();
     console.log("Neighbours:");
-    async function borderCountry(){
-        for(border of country){
-            const response = await fetch(`https://restcountries.com/v3.1/alpha/`);
-        }
-        // const neighbor = await response.json();
-        
-        //  console.log(neighbor);
-      }
-   borderCountry()
-    }
-  getBorderingCountries("MKD");
+    console.log("Albania:", borderData);
+    console.log("Bugarija:", BgData);
+    console.log("Greece:", grcData);
+    console.log("Kosovo:", unkData);
+    console.log("Srbija:", srbData);
+  });
+};
+getCounttry("MKD");
